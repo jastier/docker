31b67391960d75116ebf08f160a6a0137df25aea
Joseph Astier <jastier@gmail.com>
2020-12-16 10:33:41 -0700

    Added a JFrame App to dockerize

---
+++ Main.scala
@@ -0,0 +1,16 @@
+import java.awt.Frame
+import javax.swing.JFrame
+
+object Main extends App {
+
+
+  
+  override def main(args: Array[String])
+  {
+    println("Hello World!")
+    val foo = new Foo
+    foo.setVisible(true)
+  }
+}
+
+
